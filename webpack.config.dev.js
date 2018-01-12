f7c1e946decf7f63a63a882eeab3740e7c31eb22
chenty1 <chenty1@guahao.com>
2018-01-12 19:35:23 +0800

    修复

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,7 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 const merge = require('webpack-merge')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const baseConfig = require('./webpack.config.js')
+const baseConfig = require('./webpack.config.dev.js')
 const root = path.resolve(__dirname, '..')
 
 module.exports = merge(baseConfig, {
